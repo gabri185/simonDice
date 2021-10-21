040cf933691936384c1bb7fce9193f8747d53c01
gabri185 <gdominguezreyes@danielcastelao.org>
2021-10-21 13:05:47 +0200

    Metodos vacios creados y retoques en el layout

--- app/src/main/java/com/pmdm/primertrimestre/simondice/MainActivity.kt
+++ app/src/main/java/com/pmdm/primertrimestre/simondice/MainActivity.kt
@@ -2,10 +2,54 @@ package com.pmdm.primertrimestre.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var contadorRonda: Int =0;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("estado" , "se crea pantalla de inicio")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val botonJugar: Button = findViewById(R.id.jugar)
+        botonJugar.setOnClickListener(){
+            mostrarRonda()
+            ejecutarSecuencia()
+        }
+
+
     }
+    val tContador: TextView = findViewById(R.id.contador)
+    fun mostrarRonda(){
+        Log.d("estado" , "aparece marcador de ronda")
+        tContador.setText(contadorRonda)
+    }
+    fun ejecutarSecuencia(){
+        Log.d("estado" , "se ejecuta la secuencia")
+
+        val randomInt = (0..4).random()
+        var posicion:Int =0;
+        var array=emptyArray<Int>()
+        val drawableResource = when (randomInt) {
+            1 -> array[posicion] = 1;
+            2 -> array[posicion] = 2;
+            3 -> array[posicion] = 3;
+            else -> array[posicion] = 4;
+        }
+
+
+    }
+
+
+
+
+
+
 }
+
