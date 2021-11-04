c5638c93357b014e75c9229906d20fd86f8bf7c4
gabri185 <gdominguezreyes@danielcastelao.org>
2021-11-04 14:40:25 +0100

    arreglando errores

--- app/src/main/java/com/pmdm/primertrimestre/simondice/MainActivity.kt
+++ app/src/main/java/com/pmdm/primertrimestre/simondice/MainActivity.kt
@@ -11,7 +11,9 @@ import android.widget.Toast
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    var contadorRonda: Int =0;
+    var contadorRonda: Int = 0;
+    var secuencia = ArrayList<Int>()
+    var posicion: Int = 0;
 
     val rojo = Color.alpha(R.color.red)
     val verde = Color.alpha(R.color.green)
@@ -24,10 +26,8 @@ class MainActivity : AppCompatActivity() {
     val azulClaro = Color.parseColor("#00FFF9")
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("estado" , "se crea pantalla de inicio")
+        Log.d("estado", "se crea pantalla de inicio")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -36,126 +36,146 @@ class MainActivity : AppCompatActivity() {
         val brojo: Button = findViewById(R.id.rojo)
         val bamarillo: Button = findViewById(R.id.amarillo)
         val bazul: Button = findViewById(R.id.azul)
-        botonJugar.setOnClickListener(){
+        botonJugar.setOnClickListener() {
             mostrarRonda()
             ejecutarSecuencia()
         }
 
 
-
     }
 
-    fun mostrarRonda(){
-        contadorRonda++
+    fun mostrarRonda() {
         val tContador: TextView = findViewById(R.id.contador)
-        Log.d("estado" , "aparece marcador de ronda")
+        contadorRonda++
+        tContador.text=( "RONDA: "+contadorRonda.toString())
+        Log.d("estado", "aparece marcador de ronda")
         Toast.makeText(this, "Mostramos contador " + contadorRonda, Toast.LENGTH_LONG).show()
-        var textocontador: String = contadorRonda.toString()
-        Log.d("estado" , "ronda:" + contadorRonda)
-        tContador.setText(textocontador)
+
+        Log.d("estado", "ronda:" + contadorRonda)
+
     }
 
 
-    fun ejecutarSecuencia(){
+    fun ejecutarSecuencia() {
 
         val bverde: Button = findViewById(R.id.verde)
         val brojo: Button = findViewById(R.id.rojo)
         val bazul: Button = findViewById(R.id.azul)
         val bamarillo: Button = findViewById(R.id.amarillo)
-
         val randomInt = (1..4).random()
-        Log.d("estado" , "se ejecuta la secuencia " + randomInt)
-        var posicion:Int =0;
-        var secuencia: Array<Int> = arrayOf()
-        val job = GlobalScope.launch(Dispatchers.Main){
-            suspenderVerde(bverde, verdeClaro,  verde)
-        }
+        Log.d("estado", "se ejecuta la secuencia " + randomInt)
+
+
 
 
         //for(i in 1..contadorRonda) {
-            when (randomInt) {
+
+        when (randomInt) {
 
 
-                1 -> {
-                    val job = GlobalScope.launch(Dispatchers.Main){
-                        suspenderVerde(bverde, verdeClaro,  verde)
-                    }
-                }
+            1 -> {
+                secuencia.add(1)
+            }
+            2 -> {
+                secuencia.add(2)
+            }
+            3 -> {
+                secuencia.add(3)
+            }
+            4 -> {
+                secuencia.add(4)
+            }
 
-                2 -> {
-                    val job2 = GlobalScope.launch(Dispatchers.Main){
-                        suspenderRojo(brojo, rojoClaro,  rojo)
-                    }
-                }
-                3 -> {
-                    val job3 = GlobalScope.launch(Dispatchers.Main){
-                        suspenderAzul(bazul, azulClaro,  azul)
-                    }
-                }
-                4 -> {
-                    val job4 = GlobalScope.launch(Dispatchers.Main){
-                        suspenderAmarillo(bamarillo, amarilloClaro,  amarillo)
-                    }
-                }
-
-                /*
+            /*
                 1 -> secuencia[posicion] = 1;
                 2 -> secuencia[posicion] = 2;
                 3 -> secuencia[posicion] = 3;
                 else -> secuencia[posicion] = 4;
 
 */
+        }
+        println(secuencia)
+        //Log.d("estado" , "se guarda la secuencia " + secuencia[posicion])
+        //posicion++
+        posicion=0
+        for (i in 1..contadorRonda) {
+
+
+        when (secuencia[posicion]) {
+
+
+            1 -> {
+                val job = GlobalScope.launch(Dispatchers.Main) {
+                    suspenderVerde(bverde, verdeClaro, verde)
+                }
             }
-            //Log.d("estado" , "se guarda la secuencia " + secuencia[posicion])
-            //posicion++
+
+            2 -> {
+                val job2 = GlobalScope.launch(Dispatchers.Main) {
+                    suspenderRojo(brojo, rojoClaro, rojo)
+                }
+            }
+            3 -> {
+                val job3 = GlobalScope.launch(Dispatchers.Main) {
+                    suspenderAzul(bazul, azulClaro, azul)
+                }
+            }
+            4 -> {
+                val job4 = GlobalScope.launch(Dispatchers.Main) {
+                    suspenderAmarillo(bamarillo, amarilloClaro, amarillo)
+                }
+            }
+
+        }
+            posicion++
+
+
+
+    }
+    }
+
+        fun comprobarSecuencia(){
 
         }
 
-        /*
-        posicion ++
-
-        var x:Int =0
-
-        while ( x > array.size) {
-            val activarsecuencia = when (array[x]) {
-                1 -> "corrutina2";
-                2 -> "corrutina2";
-                3 -> "corrutina2";
-                else -> "corrutina2";
-            }
-            x++
 
 
+
+
+
+
+
+        suspend fun suspenderVerde(bverde: Button, colorChange: Int, colorDefault: Int) {
+            bverde.setBackgroundColor(colorChange)
+            delay(500L)
+            bverde.setBackgroundColor(colorDefault)
+            delay(500L)
         }
 
-*/
+        suspend fun suspenderRojo(brojo: Button, colorChange: Int, colorDefault: Int) {
+            brojo.setBackgroundColor(colorChange)
+            delay(500L)
+            brojo.setBackgroundColor(colorDefault)
+            delay(500L)
+        }
+
+        suspend fun suspenderAzul(brojo: Button, colorChange: Int, colorDefault: Int) {
+            brojo.setBackgroundColor(colorChange)
+            delay(500L)
+            brojo.setBackgroundColor(colorDefault)
+            delay(500L)
+        }
+
+        suspend fun suspenderAmarillo(bamarillo: Button, colorChange: Int, colorDefault: Int) {
+            bamarillo.setBackgroundColor(colorChange)
+            delay(500L)
+            bamarillo.setBackgroundColor(colorDefault)
+            delay(500L)
+        }
 
 
-
-    suspend fun suspenderVerde(bverde: Button, colorChange : Int, colorDefault: Int){
-        bverde.setBackgroundColor(colorChange)
-        delay(500L)
-        bverde.setBackgroundColor(colorDefault)
     }
-    suspend fun suspenderRojo(brojo: Button, colorChange : Int, colorDefault: Int){
-        brojo.setBackgroundColor(colorChange)
-        delay(500L)
-        brojo.setBackgroundColor(colorDefault)
-    }
-    suspend fun suspenderAzul(brojo: Button, colorChange : Int, colorDefault: Int){
-        brojo.setBackgroundColor(colorChange)
-        delay(500L)
-        brojo.setBackgroundColor(colorDefault)
-    }
-    suspend fun suspenderAmarillo(bamarillo: Button, colorChange : Int, colorDefault: Int){
-        bamarillo.setBackgroundColor(colorChange)
-        delay(500L)
-        bamarillo.setBackgroundColor(colorDefault)
-    }
 
-
-
-}
 
 
 
